ffa42286b59d53eae3a5df3ebee63f78822a06c3
Syed Abdullah Bin MAsood <abinemasood814@gmail.com>
2023-10-15 08:13:54 +0100

    Add project files.

---
+++ TestFor/MathTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+using Math = TestNinja.Fundamentals.Math;
+
+namespace TestNinjaUnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        private Math _math;
+        [SetUp] 
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+        
+
+        [Test]
+        
+        public void Add_WhenCalled_ReturnSumOfBothArguments()
+        { 
+            var result = _math.Add(1, 2);
+            NUnit.Framework.Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        [TestCase(2, 1, 2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(1, 1, 1)]
+        public void Max_FirstArgumentIsGreater_ReturnFirstArgument(int a, int b, int expactedvalue)
+        {
+            var result = _math.Max(a, b);
+            NUnit.Framework.Assert.That(result, Is.EqualTo(expactedvalue));
+        }
+        
+    }
+}
---
+++ TestFor/ReservationTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinjaUnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnTrue()
+        {
+            // Arrange
+            var reservation = new Reservation();
+            // Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true});
+            // Assert
+            Assert.IsTrue(result);  
+
+
+        }
+    }
+}
